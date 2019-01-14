bce8b6c055fbc51545a05bef53205496d6797219
daleblarie <DaleBLarie@gmail.com>
2019-01-13 23:14:46 -0500

    added actual number for tempo

--- app/src/main/java/com/example/dalelarie/msense2/MainActivity.java
+++ app/src/main/java/com/example/dalelarie/msense2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.dalelarie.msense2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.media.MediaPlayer;
 import android.os.Vibrator;
@@ -16,46 +18,71 @@ import android.os.CountDownTimer;
 public class MainActivity extends AppCompatActivity {
 
     private boolean sounding;
-    private int tempo;
+    private Long tempo;
     SeekBar seekBar;
+    EditText editInput;
+    String contentCheck;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editInput = (EditText) findViewById(R.id.edit_input);
 
-        tempo = 0;
+        editInput.requestFocus();
+//        tempo = 0;
         sounding = false;
-        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-                Toast.makeText(getApplicationContext(),"seekbar progress: "+progress, Toast.LENGTH_SHORT).show();
-                tempo = progress;
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                MessageBox("start");
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                MessageBox("stop");
-            }
-        });
+//        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
+//
+//        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+//
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+//                Toast.makeText(getApplicationContext(),"seekbar progress: "+progress, Toast.LENGTH_SHORT).show();
+//                tempo = progress;
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//                MessageBox("start");
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+//                MessageBox("stop");
+//            }
+//        });
     }
     /** Called when the user touches the button */
 
+    public void showKeyboard(View view){
+        InputMethodManager imm = (InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE);
+        editInput.selectAll();
+//        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(editInput, 0);
+    }
+
+    public void hideKeyboard(View view){
+        InputMethodManager imm = (InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE);
+//        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editInput.getWindowToken(), 0);
+    }
 
     //replace yourActivity.this with your own activity or if you declared a context you can write context.getSystemService(Context.VIBRATOR_SERVICE);
    public void buttonClick(View view){
+        final boolean contentFlag;
+       String content = editInput.getText().toString();
+       contentCheck = editInput.getText().toString();
+       tempo = Long.parseLong(content);
+        hideKeyboard(view);
        final View viewRef = view;
+       final long speed = (long) ((60000/tempo));
+       String message = Long.toString(speed);
+       MessageBox(message);
        final CountDownTimer countDownRef;
        sounding = !sounding;
        if(sounding){
-           countDownRef = new CountDownTimer(20000, 5000) {
+           countDownRef = new CountDownTimer(300000, speed) {
 
                public void onTick(long millisUntilFinished) {
                    if(!sounding){
@@ -71,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
            };
        countDownRef.start();
        }
-       Toast.makeText(this, Boolean.toString(sounding), Toast.LENGTH_SHORT).show();
+//       Toast.makeText(this, Boolean.toString(sounding), Toast.LENGTH_SHORT).show();
    }
     public void sendMessage(View view) {
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.ping);
@@ -81,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
         if(view.getId() == R.id.button) {
             vibe.vibrate(500);
             mp.start();
-            MessageBox("Hello World");
+//            MessageBox("Hello World");
         }
     }
     public void MessageBox(String message) {
